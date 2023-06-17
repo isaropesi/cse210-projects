31fa6fe74d27f9ca4211daea66d70e2682757bd4
isaropesi <sil22028@byui.edu>
2023-06-17 16:26:41 -0300

    Asks for user's grade percentage and displays the grade letter corresponding along with a message

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int gradePerc = int.Parse(userInput);
+        string letter;
+        string message;
+
+        if (gradePerc >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePerc >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePerc >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePerc >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (gradePerc > 69)
+        {
+            message = "Congratulations! You have passed the class";
+        }
+        else
+        {
+            message = "Keep working on it!";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+        Console.WriteLine(message);
     }
 }
