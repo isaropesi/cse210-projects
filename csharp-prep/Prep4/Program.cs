a1ba0db1770feb854261ffffc3e9c5bb25d671b6
isaropesi <sil22028@byui.edu>
2023-06-17 16:34:19 -0300

    User enters a list of numbers, then it calculates and displays the sum, average, and maximum value of them

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int newNumber = -1;
+        while (newNumber != 0)
+        {
+            Console.Write("Enter a number to add to the list (type 0 when finished): ");
+            string userInput = Console.ReadLine();
+            newNumber = int.Parse(userInput);
+
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
