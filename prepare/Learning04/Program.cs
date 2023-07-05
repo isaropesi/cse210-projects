869eb7dcf04404eeb393f4012e95a5b52b291b60
isaropesi <sil22028@byui.edu>
2023-07-04 22:45:58 -0300

    Inheritance: classes representing different kinds of homework assignments

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment b2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(b2.GetSummary());
+        Console.WriteLine(b2.GetHomeworkList());
+
+        WritingAssignment c3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(c3.GetSummary());
+        Console.WriteLine(c3.GetWritingInformation());
     }
 }
